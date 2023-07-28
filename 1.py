f2a5b29c86ec66717f96dae3c9bab1bb5d6d0e9d
larbl <labelinina@yandex.ru>
2023-07-28 23:16:17 +0500

    проект изменен

--- 1.py
+++ 1.py
@@ -1,10 +1,9 @@
 def spell(*args):
-    result = {}
-    for word in args:
-        if result.get(word[0].lower(), 0) < len(word):
-            result[word[0].lower()] = len(word)
-    return result
-
+    mas = sorted(list(map(lambda x: x.lower(), args)),key=len)
+    sl = {}
+    for i in mas:
+        sl[i[0]] = len(i)
+    return sl
 words = ['россия', 'Австрия', 'австралия', 'РумыниЯ', 'Украина', 'КИТай', 'УЗБЕКИСТАН']
 
 print(spell(*words))
